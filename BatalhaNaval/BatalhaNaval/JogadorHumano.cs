697a839979ade911507a1ce6dab7938cdf68544a
Kacy Jhony Rezende Parreiras <166572845+Kacyjhony57@users.noreply.github.com>
2024-06-13 17:25:19 -0300

    Add files via upload

---
+++ BatalhaNaval/BatalhaNaval/JogadorHumano.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatalhaNaval
+{
+    internal class JogadorHumano
+    {
+        private char[,] tabuleiro;
+        private int pontuacao;
+        private int numTirosDados;
+        private Posicao[] posTirosDados;
+        private string nickname;
+
+        public JogadorHumano (int linhas,int colunas,string nome)
+        {
+            tabuleiro = new char[linhas, colunas];
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    tabuleiro[i,j] = 'A';
+                }
+                Console.WriteLine();
+            }
+
+            pontuacao = 0;
+            numTirosDados = 0;
+         
+
+            
+        }
+
+        public string GerarNickname(string nome)
+        {
+            string[] nome2 = nome.Split(' ');
+
+            
+
+
+            return nickname;
+        }
+
+
+    }
+}
---
+++ BatalhaNaval/BatalhaNaval/Posicao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatalhaNaval
+{
+    internal class Posicao
+    {
+        private int linha;
+        private int coluna;
+
+        public Posicao (int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public int Linha
+        {
+            get { return linha; }
+            set { linha = value; }
+        }
+
+        public int Coluna
+        {
+            get { return coluna; }
+            set { coluna = value; }
+        }
+
+
+    }
+}
