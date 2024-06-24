fccc2c4b51e3b3ba05881947312c929ff33497d1
Kacy Jhony Rezende Parreiras <166572845+Kacyjhony57@users.noreply.github.com>
2024-06-24 16:04:33 -0300

    Update Embarcacao.cs

--- BatalhaNaval/BatalhaNaval/Embarcacao.cs
+++ BatalhaNaval/BatalhaNaval/Embarcacao.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,24 @@ namespace BatalhaNaval
 {
     internal class Embarcacao
     {
+        private string nome;
+        private int tamanho;
+
+        public Embarcacao(string nome, int tamanho)
+        {
+            this.nome = nome;
+            this.tamanho = tamanho;
+        }
+
+        
+        public string Nome(){
+            get { return nome;}
+            set { nome = value; }
+        }
+        public int tamanho(){
+            get { return tamanho;}
+            set { tamanho = value; }
+        }
+
     }
 }
