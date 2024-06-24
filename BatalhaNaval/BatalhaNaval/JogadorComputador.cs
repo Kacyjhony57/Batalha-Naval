52a929252ba0b28d81a5ffade809b72b26c8b64d
Kacy Jhony Rezende Parreiras <166572845+Kacyjhony57@users.noreply.github.com>
2024-06-24 16:02:49 -0300

    Update JogadorComputador.cs

--- BatalhaNaval/BatalhaNaval/JogadorComputador.cs
+++ BatalhaNaval/BatalhaNaval/JogadorComputador.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,8 +18,106 @@ namespace BatalhaNaval
             this.pontuacao = 0;
             this.numTirosDados = 0;
             this.tabuleiro = new tabuleiro[linhas,colunas];
+            
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    tabuleiro[i, j] = 'A';
+                }
+                Console.WriteLine();
+            }
+
             this.posicao = new Posicao[linhas * colunas];
 
         }
+
+        
+        
+
+        public int Pontuacao
+        {
+            get { return pontuacao;}
+            set { pontuacao = value;}
+        }
+
+        
+        public int NumTirosDados
+        {
+            get { return numTirosDados;}
+            set { numTirosDados = value;}
+        }
+
+
+        public Posicao[] EscolherAtaque()
+        {
+            int linha;
+            int coluna;
+            bool valida = false;
+
+            Randon r = new Random();
+            
+            linha = r.Next(10)
+            coluna = r.Next(10);
+            
+            Posicao tiro = new Posicao(linha, coluna);
+            
+            
+
+
+
+            return posTirosDados;
+        }
+
+        public bool ReceberAtaque(Posicao posicao)
+        {
+            bool acertou = true;                 
+
+
+            return acertou;
+        }
+
+        public void ImprimirTabuleiroJogador()
+        {
+
+        string[,] frotas = new string[9,3];
+        string[] dados;
+        string linha;
+        StreamReader arq = new StreamReader("frotaComputador.txt", Encoding.UTF8);
+        linha = arq.ReadLine();
+
+        while (linha != null)
+        {
+            dados = linha.Split(';');
+
+
+
+
+
+        }
+
+        public void ImprimirTabuleiroAdversario()
+        {
+
+        }
+        public void AdicionarEmbarcacao(Embarcacao embarcacao, Posicao posicao)
+        {
+            bool adicionar = true;
+
+
+            return adicionar;
+        }
+
+
+
+
+
+
+
+
+
+
+        
+
     }
 }
