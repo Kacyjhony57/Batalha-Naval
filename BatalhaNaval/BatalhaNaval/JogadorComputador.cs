a1b7ed31ed444670fdcb0b07ce863f4007911950
Kacy Jhony Rezende Parreiras <166572845+Kacyjhony57@users.noreply.github.com>
2024-06-24 12:24:55 -0300

    Update JogadorComputador.cs

--- BatalhaNaval/BatalhaNaval/JogadorComputador.cs
+++ BatalhaNaval/BatalhaNaval/JogadorComputador.cs
@@ -8,5 +8,18 @@ namespace BatalhaNaval
 {
     internal class JogadorComputador
     {
+        private char[,] tabuleiro;
+        private int pontuacao;
+        private int numTirosDados;
+        private Posicao[] posTirosDados;
+
+        public JogadorComputador(int linhas, int colunas)
+        {
+            this.pontuacao = 0;
+            this.numTirosDados = 0;
+            this.tabuleiro = new tabuleiro[linhas,colunas];
+            this.posicao = new Posicao[linhas * colunas];
+
+        }
     }
 }
