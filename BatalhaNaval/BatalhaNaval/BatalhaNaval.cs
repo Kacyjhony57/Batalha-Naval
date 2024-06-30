0b980952c9277708b3c1917e473be37b4ecea5fd
Kacy Jhony Rezende Parreiras <166572845+Kacyjhony57@users.noreply.github.com>
2024-06-30 14:23:09 -0300

    Update BatalhaNaval.cs

--- BatalhaNaval/BatalhaNaval/BatalhaNaval.cs
+++ BatalhaNaval/BatalhaNaval/BatalhaNaval.cs
@@ -1,17 +1,136 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BatalhaNaval
 {
+
+    static void TabuleiroComputador()
+    {
+        string linha;
+        string[] lerlinhas;
+
+        try
+        {
+            
+            StreamReader arq = new StreamReader("frotaComputador.txt", Encoding.UTF8);
+            linha = arq.ReadLine();
+
+            while (linha != null)
+            {
+                lerlinhas = linha.Split(';');
+                
+
+                linha = arq.ReadLine();
+            }
+            arq.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro: " + ex.Message);
+        }
+
+
+
+    }
+
+
     internal class BatalhaNaval
     {
         static void Main(string[] args)
         {
-            JogadorHumano h = new JogadorHumano(10, 10, "Kelvin Lemos");
-            JogadorComputador c = new JogadorComputador(10,10);
+            Console.WriteLine("\tJOGO BATALHA NAVAL");
+            string nomeJ;
+            Console.Write("Digite o nome do jogador: ");
+            nomeJ = Console.ReadLine();
+            JogadorHumano h = new JogadorHumano(10, 10, nomeJ);
+            string nick = h.GerarNickname(nomeJ);
+            JogadorComputador c = new JogadorComputador(10, 10);
+            h.ImprimirTabuleiroJogador();
+            Embarcacao[] barcos = new Embarcacao[9];
+            barcos[0] = new Embarcacao("Submarino-1", 1);
+            barcos[1] = new Embarcacao("Submarino-2", 1);
+            barcos[2] = new Embarcacao("Submarino-3", 1);
+            barcos[3] = new Embarcacao("Hidroavião-1", 2);
+            barcos[4] = new Embarcacao("Hidroavião-2", 2);
+            barcos[5] = new Embarcacao("Cruzador-1", 3);
+            barcos[6] = new Embarcacao("Cruzador-2", 3);
+            barcos[7] = new Embarcacao("Encouraçado", 4);
+            barcos[8] = new Embarcacao("Porta-aviões", 5);
+            
+            for (int i = 0; i < barcos.Length; i++)
+            {
+                bool done = false;
+                while (done == false)
+                {
+                    int linha, coluna;
+                    Console.WriteLine("Informe onde deseja posicionar o " + barcos[i].Nome + "  de tamanho: " + barcos[i].Tamanho);
+                    Console.WriteLine("Informe a linha: ");
+                    linha = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Informe a coluna: ");
+                    coluna = int.Parse(Console.ReadLine());
+                    Posicao jogada = new Posicao(linha, coluna);
+                    done = h.AdicionarEmbarcacao(barcos[i],jogada);
+                }
+            }
+            //mostrar o tabuleiro do computador
+            //
+            //
+            //
+            bool ganhou = false;
+            for(int i= 0 ; i < 100 || ganhou == false ; i++)
+            {
+                int pontoC = 0;
+                int pontoH = 0;
+                string campeao;
+                bool acerto = true; 
+                while(acerto)
+                {
+                    Posicao[] tiro = h.EscolherAtaque();
+                    acerto = c.ReceberAtaque(tiro);
+                    if(acerto == true)
+                    {
+                        pontoH++;
+                        if(pontoH == 22)
+                        {
+                        campeao = $"Parabéns! ${nick} venceu!";
+                        ganhou = true;
+                        }
+                    }
+
+                }
+                acerto = true;
+                while(acerto)
+                {
+                    Posicao tiro = c.EscolherAtaque();
+                    acerto = h.ReceberAtaque(tiro);
+                    if(acerto == true)
+                    {
+                        pontoC++;
+                        if(pontoC == 22)
+                        {
+                        campeao = "Computador Ganhou!";
+                        ganhou = true;
+                        }
+                    }
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
